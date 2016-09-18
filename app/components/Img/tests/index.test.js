2692baca6ebe034a173b5cc6bb733600d675f505
mydearxym <mydearxym@qq.com>
2016-09-18 16:35:48 +0800

    update to 0.2.0

--- app/components/Img/tests/index.test.js
+++ app/components/Img/tests/index.test.js
@@ -4,14 +4,41 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import React from 'react';
 
+const src = 'test.png';
+const alt = 'test';
+const renderComponent = (props = {}) => shallow(
+  <Img
+    alt={alt}
+    src={src}
+    {...props}
+  />
+);
+
 describe('<Img />', () => {
   it('should render an <img> tag', () => {
-    const renderedComponent = shallow(<Img alt="test" src="test.png" />);
+    const renderedComponent = renderComponent();
     expect(renderedComponent).to.have.tagName('img');
   });
 
+  it('should have an src attribute', () => {
+    const renderedComponent = renderComponent();
+    expect(renderedComponent).to.have.attr('src', src);
+  });
+
   it('should have an alt attribute', () => {
-    const renderedComponent = shallow(<Img alt="test" src="test.png" />);
-    expect(renderedComponent).to.have.attr('alt', 'test');
+    const renderedComponent = renderComponent();
+    expect(renderedComponent).to.have.attr('alt', alt);
+  });
+
+  it('should adopt a className attribute', () => {
+    const className = 'test';
+    const renderedComponent = renderComponent({ className });
+    expect(renderedComponent).to.have.attr('class', className);
+  });
+
+  it('should not adopt a srcset attribute', () => {
+    const srcset = 'test-HD.png 2x';
+    const renderedComponent = renderComponent({ srcset });
+    expect(renderedComponent).to.not.have.attr('srcset', srcset);
   });
 });
