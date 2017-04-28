b88f9dc08288cfd1426a54be8fdb820d93463926
mydearxym <mydearxym@qq.com>
2017-04-28 17:02:47 +0800

    wip
    https://github.com/react-boilerplate/react-boilerplate/pull/1134/files

--- app/components/List/tests/index.test.js
+++ app/components/List/tests/index.test.js
@@ -1,5 +1,5 @@
-import { shallow } from 'enzyme';
 import React from 'react';
+import { shallow } from 'enzyme';
 
 import ListItem from 'components/ListItem';
 import List from '../index';
@@ -12,17 +12,17 @@ describe('<List />', () => {
     expect(renderedComponent.find(ListItem)).toBeDefined();
   });
 
-  it('should render the items', () => {
+  it('should pass all items props to rendered component', () => {
     const items = [
       { id: 1, name: 'Hello' },
       { id: 2, name: 'World' },
     ];
+
     const component = ({ item }) => <ListItem>{item.name}</ListItem>; // eslint-disable-line react/prop-types
 
     const renderedComponent = shallow(
-      <List component={component} items={items} />
+      <List items={items} component={component} />
     );
-
     expect(renderedComponent.find(component)).toHaveLength(2);
     expect(renderedComponent.find(component).at(0).prop('item')).toBe(items[0]);
     expect(renderedComponent.find(component).at(1).prop('item')).toBe(items[1]);
